3c5536ef001937f1589213832e4d44243c91e60a
lucashtarifa <162587882+lucashtarifa@users.noreply.github.com>
2024-08-08 07:43:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const caixaprincipal = document.querySelector('.caixa-principal');
+const caixapergunta = document.querySelector('.caixa-pergunta');
+const caixaalternativa = document.querySelector('.caixa-alternativa');
+const caixaresultado = document.querySelector('.caixa-resultado');
+
+const pergunta = [ //abre a lista de objetos/itens
+    { //abre o item
+        enunciado: "pergunta 1",
+        alternativa: ["Alternativa 1" ,"Alternativa 2"]
+    },
+
+    { //abre o item
+        enunciado: "pergunta 12",
+        alternativa: ["Alternativa 1" ,"Alternativa 2"]
+    },
+
+    { //abre o item
+        enunciado: "pergunta 12",
+        alternativa: ["Alternativa 1" ,"Alternativa 2"]
+    },
+]
+
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+
+}
+
